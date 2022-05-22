e195b23b313ada0fa27d58ddf5bb0708777de137
LucasRehmann <101809208+LucasRehmann@users.noreply.github.com>
2022-05-22 17:08:55 -0300

    Initial commit

---
+++ Assets/Scripts/ValidarContrasena.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContrasena : MonoBehaviour
+{
+
+    string contrasenaCorrecta;
+    string contrasenaUsuario;
+    public Text ingresoUsuario;
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        contrasenaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void validarContrasena()
+    {
+        contrasenaUsuario = ingresoUsuario.text;
+        if (contrasenaUsuario == contrasenaCorrecta)
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+
+        else
+        {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
+            Debug.Log("Contraseña Incorrecta");
+        }
+    }
+}
